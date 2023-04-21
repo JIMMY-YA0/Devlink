4118d968ab5a81563957e7f49a51870aab500844
Jimmy Yao <jimmyyaocs@gmail.com>
2023-04-21 21:48:07 +1000

    feat: display the list of links

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,17 @@ import Image from 'next/image'
 import { useEffect, useState } from 'react'
 import supabase from '@/utils/supabaseClient'
 
+type Link = {
+  title: string
+  url: string
+}
+
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
   const [useId, setUserId] = useState<string | undefined>()
   const [title, setTitle] = useState<string | undefined>()
   const [url, setUrl] = useState<string | undefined>()
+  const [links, setLinks] = useState<Link[]>()
 
   useEffect(() => {
     const getUser = async () => {
@@ -20,6 +26,22 @@ export default function Home() {
     }
     getUser()
   }, [])
+
+  useEffect(() => {
+    const getLinks = async () => {
+      try {
+        const { data, error } = await supabase
+          .from('links')
+          .select('title, url')
+          .eq('user_id', useId)
+        if (error) throw error
+        setLinks(data)
+      } catch (error) {
+        console.log('error: ', error)
+      }
+    }
+    if (useId) getLinks()
+  }, [useId])
 
   const addNewLink = async () => {
     try {
@@ -42,6 +64,13 @@ export default function Home() {
 
   return (
     <div className="flex flex-col w-full justify-center items-center mt-4">
+      {links?.map((link: Link, index: number) => (
+        <div className="mt-4" key={index}>
+          <a href={link.url} target="_blank" rel="noreferrer">
+            {link.title}
+          </a>
+        </div>
+      ))}
       {isAuthenticated && (
         <>
           <div className="mt-4">
