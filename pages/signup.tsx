07742111c00477f3e8c8cd18f5fd0e258f82f06f
Jimmy Yao <jimmyyaocs@gmail.com>
2023-04-21 17:36:27 +1000

    feat: add signUp authentication

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,8 +1,23 @@
+import supabase from '@/utils/supabaseClient'
 import { useState } from 'react'
 
 const Signup = () => {
   const [email, setEmail] = useState<string | undefined>()
   const [password, setPassword] = useState<string | undefined>()
+
+  async function signUpWithEmail() {
+    try {
+      if (email && password) {
+        const response = await supabase.auth.signUp({ email, password })
+        console.log('response: ', response)
+        if (response.error) throw response.error
+        const userId = response.data.user?.id
+        console.log('UserId: ', userId)
+      }
+    } catch (error) {
+      console.log('error: ', error)
+    }
+  }
 
   return (
     <>
@@ -16,6 +31,7 @@ const Signup = () => {
             id="email"
             className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
             placeholder="you@example.com"
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -28,12 +44,14 @@ const Signup = () => {
             id="password"
             className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
             placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
 
         <button
           type="button"
           className="inline-flex items-center rounded-md border border-transparent bg-indigo-600 text-white font-medium hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mt-4 px-4 py-2 text-sm "
+          onClick={signUpWithEmail}
         >
           Sign up
         </button>
